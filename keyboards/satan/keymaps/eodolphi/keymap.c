eaa8090f76849fb2f66f7c1dc1fe7c72070c4144
Ernst Odolphi <ernst@onepercentclub.com>
2017-11-21 15:19:44 +0100

    Small fixes

--- keyboards/satan/keymaps/eodolphi/keymap.c
+++ keyboards/satan/keymaps/eodolphi/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   MO(VIM_LAYER), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
-  OSM(MOD_LSFT),         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
+  OSM(MOD_LSFT), MO(VIM_LAYER),        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
   KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 MO(VIM_LAYER),KC_RALT, KC_RCTL, TG(VIM_LAYER)
   ),
   /* Keymap FUN_LAYER: Function Layer
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,_______,  \
   _______,_______,LALT(KC_RGHT),_______,BL_TOGG,RESET,LGUI(KC_C),LGUI(KC_Z),MO(VIM_LAYER),_______,LGUI(KC_V), _______,KC_PGDN, KC_PGUP, \
   _______,MO(VIM_LAYER),_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,_______, \
-  _______,_______,KC_DEL,_______,_______,LALT(KC_LEFT),KC_VOLD,KC_VOLU,KC_MUTE,_______,LGUI(KC_F),_______, _______, \
+  _______,_______,_______,KC_DEL,_______,_______,LALT(KC_LEFT),KC_VOLD,KC_VOLU,KC_MUTE,_______,LGUI(KC_F),_______, _______, \
   _______,_______,_______,                 _______,                       _______,_______,_______),
 };
 
